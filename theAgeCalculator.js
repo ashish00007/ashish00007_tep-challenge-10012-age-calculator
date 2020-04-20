183483e005366b7ecf2d0c4f9145bdce7bc2c43a
ashish00007 <ashishtyagi8130@gmail.com>
2020-04-20 15:48:49 +0530

    minim js1

---
+++ theAgeCalculator.js
@@ -0,0 +1,8 @@
+var birthYear = 1988;
+var futureYear = 2026;
+
+var age = futureYear - birthYear;
+
+var secondAge = age-1;
+
+console.log('i will be either '+secondAge+' or '+age+' in '+ futureYear);
---
+++ theFortuneTeller.js
@@ -0,0 +1,9 @@
+var noOfChildren = 2;
+var partnerName = 'selena';
+var geographicLocation = 'delhi';
+var jobTitle = 'computer engineer';
+
+console.log('You will be a '+ jobTitle + ' in ' + geographicLocation + ', and married to ' + partnerName + ' with ' + noOfChildren + ' kids');
+
+
+
